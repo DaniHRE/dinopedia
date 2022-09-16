ed9a6aa42773a0af36f05d4eaadc1c23bb9f2138
Daniel Henrique <henriqueevaldo@outlook.com>
2022-09-16 16:02:45 -0300

    core and basic models

--- dinopedia/models.py
+++ dinopedia/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Dinosaur(models.Model):
+    reino = models.CharField(max_length=255)
+    filo = models.CharField(max_length=255)
+    clado = models.CharField(max_length=255)
+    familia = models.CharField(max_length=255)
+    subFamilia = models.CharField(max_length=255)
+    genero = models.CharField(max_length=255)
+
+class Category(models.Model):
+    
