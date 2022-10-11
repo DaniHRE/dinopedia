2f39f1f1f92f0c0b5ca9da04b3d689b616af8962
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-11 14:36:49 -0300

    create routes 🛑

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { render } from 'react-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import { Home } from './pages/Home';
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      {/* <Route path="*" element={}/> */}
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div className="Home">
+      <h1>Dinopedia</h1>
+    </div>
+  )
+}
