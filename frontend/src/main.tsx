90717ec3720241d33a935bab9fd39ec003524b6e
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-11 15:41:30 -0300

    add routes ✅

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,14 @@ import { render } from 'react-dom';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
+import { PageNotFound } from './pages/PageNotFound';
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      {/* <Route path="*" element={}/> */}
+      <Route path="*" element={<PageNotFound/>}/>
     </Routes>
   </BrowserRouter>,
   rootElement
