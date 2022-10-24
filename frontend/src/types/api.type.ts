dfb0e88472b7d1f913c78eda0e3160301da35635
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-24 16:02:29 -0300

    source: add api types

---
+++ frontend/src/types/api.type.ts
@@ -0,0 +1,13 @@
+export default interface IDinosaurData {
+    id: number,
+    image: string,
+    nome: string,
+    reino: string,
+    filo: string,
+    clado: string,
+    familia: string,
+    subFamilia: string,
+    genero: string,
+    category: number,
+}
+
