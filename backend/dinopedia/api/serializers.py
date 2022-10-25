98f3a8b45d8fe2dfd12880f8f7ca75843e51e2e3
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-25 11:32:29 -0300

    source: update serializer return

--- backend/dinopedia/api/serializers.py
+++ backend/dinopedia/api/serializers.py
@@ -1,19 +1,28 @@
+from asyncore import read
 from rest_framework import serializers
 from ..models import Dinosaur
 from ..models import Category
-
-class DinosaurSerializer(serializers.ModelSerializer):
-    
-    image = serializers.ImageField(required=True)
-    
-    class Meta:
-        model = Dinosaur
-        fields = "__all__"
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
 
+class DinosaurSerializer(serializers.ModelSerializer):
+    
+    image = serializers.ImageField(required=True)
 
+    class Meta:
+        model = Dinosaur
+        fields = [
+            'image',
+            'name',
+            'species',
+            'category',
+            'feeding_habit',
+            'height',
+            'length',
+            'weight',
+            'country',
+            'region',
+            'description']
