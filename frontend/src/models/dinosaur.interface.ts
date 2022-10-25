fb4b69e570907d79574e734f041889838e287153
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-25 11:34:44 -0300

    type: update data interface

--- frontend/src/models/dinosaur.interface.ts
+++ frontend/src/models/dinosaur.interface.ts
@@ -1,12 +1,14 @@
 export interface DinosaurType {
     id: number;
-    nome: string;
-    clado: string;
-    filo: string;
-    genero: string;
-    reino: string;
-    familia: string;
-    subFamilia: string;
     image: string;
+    name: string;
+    species: string;
     category: number;
+    feeding_habit: string;
+    height: string;
+    length: string;
+    weight: string;
+    country: string;
+    region: string;
+    description: string;
 }
