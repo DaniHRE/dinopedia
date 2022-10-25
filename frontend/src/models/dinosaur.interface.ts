95eefc61c9bf1034b51ae294aea1228a11406b51
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-25 07:53:19 -0300

    source: change interface, type to models folder

---
+++ frontend/src/models/dinosaur.interface.ts
@@ -0,0 +1,12 @@
+export interface Dinosaur {
+    id: number;
+    nome: string;
+    clado: string;
+    filo: string;
+    genero: string;
+    reino: string;
+    familia: string;
+    subFamilia: string;
+    image: string;
+    category: number;
+}
