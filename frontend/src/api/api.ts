16d562f39c1ea671baca78e71a2eeb009818a309
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-25 08:28:55 -0300

    source: add api controller

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosResponse } from 'axios';
+import { DinosaurType } from '../models/dinosaur.interface';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8000/api/',
+    timeout: 15000,
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+    get: (url: string) => instance.get(url).then(responseBody),
+    post: (url: string, body: {}) => instance.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => instance.put(url, body).then(responseBody),
+    delete: (url: string) => instance.delete(url).then(responseBody),
+};
+
+export const Dinosaur = {
+    getDinosaurs: (): Promise<DinosaurType> => requests.get('dinosaur'),
+    getDinosaur: (id: number): Promise<DinosaurType> => requests.get(`dinosaur/${id}`),
+    createDinosaur: (dinosaur: DinosaurType): Promise<DinosaurType> => requests.post('dinosaurs', dinosaur),
+    updateDinosaur: (dinosaur: DinosaurType, id: number): Promise<DinosaurType> => requests.put(`dinosaur/${id}`, dinosaur),
+    deleteDinosaur: (id: number): Promise<void> => requests.delete(`dinosaur/${id}`),
+};
