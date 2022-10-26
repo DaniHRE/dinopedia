37f21ce4b78273665306b0055cf23ebd2be1a104
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-26 17:04:53 -0300

    feat: post modal

---
+++ frontend/src/components/CreateModal/index.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { useStyles } from './style';
+import { useForm } from '@mantine/form';
+import { IconPlus } from '@tabler/icons';
+import { DinosaurType } from '../../models/dinosaur.interface';
+import { Modal, Button, Group, Title, DefaultProps, ActionIcon, TextInput, Textarea, NumberInput } from '@mantine/core';
+
+export interface CreateModalProps extends DefaultProps {
+    onOpen: () => void;
+    setIsEdit: (state: boolean) => void;
+    dinosaur: DinosaurType;
+}
+
+export function CreateModal({ onOpen, setIsEdit, dinosaur }: CreateModalProps) {
+    const [opened, setOpened] = useState(false);
+    const { classes } = useStyles();
+
+    const form = useForm({
+        initialValues: {
+            name: '',
+            species: '',
+            feeding_habit: '',
+            height: '',
+            length: '',
+            weight: '',
+            region: '',
+            short_description: '',
+            description: ''
+        }
+    });
+
+    return (
+        <>
+            <Modal
+                opened={opened}
+                onClose={() => {setOpened(false); form.reset()}}
+                title={
+                    <Title order={3} weight={500}>
+                        Add Dinosaur 🦖
+                    </Title>
+                }
+            >
+                <Group grow mt="sm">
+                    <TextInput withAsterisk label="Name" placeholder="Name" {...form.getInputProps('name')} />
+                    <TextInput withAsterisk label="Species" placeholder="Species" {...form.getInputProps('species')} />
+                </Group>
+                <Group grow mt="sm">
+                    <TextInput withAsterisk label="Feeding Habit" placeholder="Feeding Habit" {...form.getInputProps('feeding_habit')} />
+                    <TextInput withAsterisk label="Region" placeholder="Region" {...form.getInputProps('region')} />
+                </Group>
+                <Group grow mt="sm">
+                    <NumberInput withAsterisk label="Height in m" placeholder="Height" {...form.getInputProps('height')} />
+                    <NumberInput withAsterisk label="Length in m" placeholder="Length" {...form.getInputProps('length')} />
+                    <NumberInput withAsterisk label="Weight in kg" placeholder="Weight" {...form.getInputProps('weight')} />
+                </Group>
+                <Group grow mt="sm">
+                    <Textarea autosize withAsterisk label="Short Description" {...form.getInputProps('short_description')} />
+                    <Textarea autosize withAsterisk label="Description" {...form.getInputProps('description')} />
+                </Group>
+
+                <Group mt="xl" position="right">
+                    <Button> Confirm </Button>
+                </Group>
+            </Modal>
+
+            <Group position="center">
+                <ActionIcon onClick={() => setOpened(true)} variant="default" radius="md" size={36}>
+                    <IconPlus size={18} stroke={1.5} />
+                </ActionIcon>
+            </Group>
+        </>
+    );
+}
