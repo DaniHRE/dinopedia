51593d268c7e091ff7fda296156f691300d0056c
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-01 14:18:53 -0300

    fix: improve delete modal

--- frontend/src/components/DeleteModal/index.tsx
+++ frontend/src/components/DeleteModal/index.tsx
@@ -1,42 +1,30 @@
-import { useState } from 'react';
 import { useStyles } from './style';
-import { IconTrash } from '@tabler/icons';
-import { Modal, Button, Group, Title, DefaultProps, ActionIcon, TextInput, Textarea, NumberInput, FileButton } from '@mantine/core';
-import { Dinosaur } from '../../api/api';
+import { Modal, Button, Title, DefaultProps, Group, ActionIcon } from '@mantine/core';
 
 export interface DeleteModalProps extends DefaultProps {
-    id: number;
-    // onOpen: () => void;
-    // setIsEdit: (state: boolean) => void;
-    // dinosaur: DinosaurType;
+    dinosaurID: number;
+    isOpen: boolean;
+    onToggle: () => void;
+    onRemove: () => void;
 }
 
-export function DeleteModal({ id, onOpen, setIsEdit, dinosaur }: DeleteModalProps) {
-    const [opened, setOpened] = useState(false);
-    const [file, setFile] = useState<File | null>(null);
+export function DeleteModal({ dinosaurID, isOpen, onToggle, onRemove }: DeleteModalProps) {
     const { classes } = useStyles();
 
     return (
-        <>
-            <Modal
-                opened={opened}
-                overlayOpacity={0.55}
-                overlayBlur={3}
-                onClose={() => { setOpened(false) }}
-                title={
-                    <Title order={3} weight={500}>
-                        Remove Dinosaur 🦖☄
-                    </Title>
-                }
-            >
-                <Button onClick={() => { Dinosaur.deleteDinosaur(id) }} />
-            </Modal>
-
-            <Group position="center">
-                <ActionIcon onClick={() => setOpened(true)} variant="default" radius="md" size={36}>
-                    <IconTrash size={18} stroke={1.5} />
-                </ActionIcon>
-            </Group>
-        </>
+        <Modal
+            opened={isOpen}
+            overlayOpacity={0.55}
+            overlayBlur={3}
+            onClose={() => { onToggle() }}
+            title={
+                <Title order={3} weight={500}>
+                    Remove Dinosaur 🦖☄
+                    {`${dinosaurID}`}
+                </Title>
+            }
+        >
+            <Button onClick={() => {onRemove(); onToggle()}} />
+        </Modal>
     );
 }
